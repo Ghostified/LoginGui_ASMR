1e3dea3769d58595b75e2bfce0ad432ce02a5e90
Allan Branson <bransonaallan@gmail.com>
2024-03-10 23:58:09 +0300

    10:22  image and username textfield

--- src/CommonConstants.java
+++ src/CommonConstants.java
@@ -2,10 +2,11 @@ import java.awt.*;
 
 public class CommonConstants {
     //File paths
-    public static final String LOGIN_IMAGE_PATH = "src/ImageFiles/LoginImage.png";
+    public static final String LOGIN_IMAGE_PATH = "C:\\Users\\allan.branson\\Projects\\LoginGui_ASMR\\src\\ImageFiles\\LoginImage.png";
 
     //frame config
     public static final Dimension FRAME_SIZE = new Dimension (540, 760);
+    public static final Dimension TEXTFIELD_SIZE = new Dimension((int)(FRAME_SIZE.width*0.80),50);
 
     //login config
     public static final  Dimension LOGIN_IMAGE_SIZE = new Dimension(255, 262);
--- src/Custom/TextFieldCustom.java
+++ src/Custom/TextFieldCustom.java
@@ -1,6 +1,9 @@
 package Custom;
 
-public class TextFieldCustom {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class TextFieldCustom extends JTextField {
     private String placeHolderText ;
     private boolean hasPlaceHolder;
     public boolean isHasPlaceHolder() {
@@ -16,6 +19,49 @@ public class TextFieldCustom {
         hasPlaceHolder = true;
 
         //Limit char input in the field
+        setDocument(new LimitText(charLimit));
+        setText(this.placeHolderText);
 
+        addListeners();
+    }
+    private void addListeners() {
+        //check for mouse clicks
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (hasPlaceHolder) {
+                    hasPlaceHolder = false;
+                    setText("");
+                }
+            }
+        });
+
+        //check for key presses
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (hasPlaceHolder) {
+                    hasPlaceHolder = false;
+                    setText("");
+                }
+            }
+        });
+
+        //check to see if user has removed focus for this field
+        addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                // check if it is empty
+                if (getText().toString().length() <= 0) {
+                    hasPlaceHolder = true;
+                    setText(placeHolderText);
+                }
+            }
+        });
     }
 }
