9dcab8859f856b4ca788c966f9436976ccf31fbc
Allan Branson <bransonaallan@gmail.com>
2024-03-10 22:11:25 +0300

    creating a jframe

---
+++ src/LoginGui.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class LoginGui extends JFrame {
+    public LoginGui() {
+        super ("Tap Tap Ind . Login");
+        setSize (CommonConstants.FRAME_SIZE);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLayout(null);
+    }
+}
